da831195435e6c3844cdc6fa44e0e459049c0b8c
César Ferreira <cesarferreiraa67@gmail.com>
2021-07-25 20:32:50 -0300

    Instalação template AdminLte

---
+++ backend/views/layouts/left.php
@@ -0,0 +1,44 @@
+<?php
+use yii\bootstrap\Nav;
+
+?>
+<aside class="main-sidebar">
+
+    <section class="sidebar">
+
+        
+        <!-- search form -->
+        <form action="#" method="get" class="sidebar-form">
+            <div class="input-group">
+                <input type="text" name="q" class="form-control" placeholder="Search..."/>
+              <span class="input-group-btn">
+                <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i>
+                </button>
+              </span>
+            </div>
+        </form>
+        <!-- /.search form -->
+
+        <?=
+        Nav::widget(
+            [
+                'encodeLabels' => false,
+                'options' => ['class' => 'sidebar-menu'],
+                'items' => [
+                    '<li class="header">Menu Yii2</li>',
+                    ['label' => '<i class="fa fa-file-code-o"></i><span>Categorias</span>', 'url' => ['/categorias']],
+                    ['label' => '<i class="fa fa-dashboard"></i><span>Postagens</span>', 'url' => ['/postagens']],
+                    [
+                        'label' => '<i class="glyphicon glyphicon-lock"></i><span>Sing in</span>', //for basic
+                        'url' => ['/site/login'],
+                        'visible' =>Yii::$app->user->isGuest
+                    ],
+                ],
+            ]
+        );
+        ?>
+
+       
+    </section>
+
+</aside>
